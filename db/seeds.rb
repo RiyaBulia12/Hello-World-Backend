441394692886a3e15f117ea0c65f7ff8149684fc
riyabulia12 <riyabulia12@gmail.com>
2023-01-22 10:52:42 +0530

    Generate greeting table, seeds and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create!([
+  { text: "Hello" },
+  { text: "Hi" },
+  { text: "Howdy" },
+  { text: "Hey" },
+  { text: "What's up?" },
+])
